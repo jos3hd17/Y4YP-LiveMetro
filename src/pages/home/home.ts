9e32f51f25c79041cb5a2ea74eeb1fca1d61d957
Jose David Hernandez Diaz <jose.k17@hotmail.com>
2017-09-23 15:51:00 -0500

    PolyLine

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -30,7 +30,7 @@ googleMap(){
      var place ={lat: latitude, lng: longitude};
      var mapDiv = document.getElementById('map');
         var map = new google.maps.Map(mapDiv, {
-          zoom: 18,
+          zoom: 10,
           center: place
         });
         var marker = new google.maps.Marker({
@@ -38,15 +38,21 @@ googleMap(){
           map: map,
           label: this.ubi.nombre
         });
-   /*var destinations = [];
-      destinations.push(new google.maps.LatLng(6.337804,-75.544299)); //Niquia
-      destinations.push(new google.maps.LatLng(6.330673, -75.553296));  //Bello
-      destinations.push(new google.maps.LatLng(6.315869, -75.555376)); //Madera
-      destinations.push(new google.maps.LatLng(6.300145,-75.558536)); //Acevedo
-               
-      var polyLineOptions ={path : destinations};
-      var polyLine = new google.maps.PolyLine(polyLineOptions);
-      polyLine.setmap(map);
-              */  
+   var stations = [];
+      stations.push(new google.maps.LatLng(6.337804,-75.544299)); //Niquia
+      stations.push(new google.maps.LatLng(6.330673, -75.553296));  //Bello
+      stations.push(new google.maps.LatLng(6.315869, -75.555376)); //Madera
+      stations.push(new google.maps.LatLng(6.300145,-75.558536)); //Acevedo
+      stations.push(new google.maps.LatLng(6.290361,-75.564716)); //Tricentenario
+      stations.push(new google.maps.LatLng(6.278235,-75.569479)); // Caribe
+      stations.push(new google.maps.LatLng(6.274641,-75.569876)); //Intermedio Caribe
+      stations.push(new google.maps.LatLng(6.270716,-75.569190)); //Intermedio caribe-universidad
+      stations.push(new google.maps.LatLng(6.269350,-75.565791)); //Universidad 
+      
+
+      var polyLineOptions ={path : stations};
+      var polyLine = new google.maps.Polyline(polyLineOptions);
+      polyLine.setMap(map);
+            
   }
 }
